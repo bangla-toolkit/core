e5b23410f4c65a0d7060b8ba2dec6e71fcb37e34
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-03-24 17:26:49 +0600

    Deploy website - based on 4d05cddf73d9545c9d0917a84ef6563b6f925673

---
+++ assets/js/5e95c892.b8ae07ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_bntk_docs=self.webpackChunk_bntk_docs||[]).push([[647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var c=r(4164),n=r(1769),t=r(204),u=r(2831),a=r(4582),d=r(4848);function k(e){return(0,d.jsx)(n.e3,{className:(0,c.A)(t.G.wrapper.docsPages),children:(0,d.jsx)(a.A,{children:(0,u.v)(e.route.routes)})})}}}]);
